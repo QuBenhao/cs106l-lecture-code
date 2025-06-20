73d0eed339c89e28197b18b95137fff3e8d0a370
qubh <qubenhao2@gmail.com>
2025-06-20 11:23:14 +0800

    feat: algorithms

--- lecture08/practice.cpp
+++ lecture08/practice.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <random>
 #include <string>
 #include <sstream>
 #include <stdexcept>
@@ -8,6 +9,7 @@
 #include <vector>
 #include <list>
 #include <typeinfo>
+#include <functional>
 #include <unordered_map>
 
 class Time {
@@ -97,8 +99,14 @@ int main() {
     std::list<int> lst{1, 2, 3, 4, 5, 6, 7, 8, 9};
     int count = countOccurrences(lst.begin(), lst.end(), lessThanFive);
 
+    // auto countFunc = std::bind(countOccurrences, std::placeholders::_1, std::placeholders::_2, lessThanFive);
+
     std::cout << "Count of elements less than 5 in list: " << count << std::endl;
     std::vector<Time> times = {Time(1, 30), Time(2, 15), Time(0, 45)};
+    std::shuffle(times.begin(), times.end(), std::mt19937{std::random_device{}()});
+
+    std::copy(times.begin(), times.end(), std::ostream_iterator<Time>(std::cout, ", "));
+    std::cout << std::endl;
     int timeCount = countOccurrences(times.begin(), times.end(), [](const Time& t) { return t == Time(1, 30); });
     std::cout << "Count of Time(1, 30) in vector: " << timeCount << std::endl;
     timeCount = countOccurrences(times.begin() + 1, times.end(), [](const Time& t) { return t == Time(1, 30); });
@@ -112,4 +120,40 @@ int main() {
     // int wordOccurrences = countOccurrences(wordCount.begin(),
     // wordCount.end(), std::make_pair(std::string("banana"), 2));
     // std::cout << "Count of ('banana', 2) in unordered_map: " << wordOccurrences << std::endl;
+
+    // std::sort, std::nth_element, std::stable_partition, std::copy_if, std::remove_if
+    std::vector<int> vec{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+    std::cout << "Before sorting: ";
+    std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+    std::sort(vec.begin(), vec.end());
+    std::cout << "After sorting: ";
+    std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+    auto cmpFunc = [](int a, int b) { return a > b; };
+    // int median = std::nth_element(vec.begin(), vec.begin() + vec.size() / 2, vec.end(), cmpFunc);
+    // std::cout << "Median value: " << median << std::endl;
+    std::stable_partition(vec.begin(), vec.end(), [](int x) { return x > 4; });
+    std::cout << "After stable partitioning (elements > 4): ";
+    std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+
+    std::vector<int> filteredVec;
+    std::copy_if(vec.begin(), vec.end(), std::back_inserter(filteredVec), [](int x) { return x < 5; });
+    std::cout << "Filtered vector (elements < 5): ";
+    std::copy(filteredVec.begin(), filteredVec.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+
+    auto end = std::remove_if(vec.begin(), vec.end(), [](int x) { return x < 5; });
+    vec.erase(end, vec.end());
+    std::cout << "After removing elements < 5: ";
+    std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+
+    std::string s = "Hello, World!";
+    std::string subStr = "World";
+    // auto it = std::search(s.begin(), s.end(), std::string("World"));
+    auto it = std::search(s.begin(), s.end(), subStr.begin(), subStr.end());
+    std::cout << "Substring found at position: " << std::distance(s.begin(), it) << std::endl;
+
 }
